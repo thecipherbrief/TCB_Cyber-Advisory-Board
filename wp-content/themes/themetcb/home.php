7f553b5793c291a375ebcd613b07eff94d40366c
Matthew Filbert <matthewfilbert@Williams-MacBook-Pro.local>
2017-10-12 17:32:08 -0400

    categorical sep of three main article types

--- wp-content/themes/themetcb/home.php
+++ wp-content/themes/themetcb/home.php
@@ -11,26 +11,74 @@
     <h1>Our Latest News</h1>
     <?php
 // the query
-$wpb_all_query = new WP_Query(array(
-  'post_type' =>'post',
-  'post_status' =>'publish',
-  'posts_per_page' => 4,
-  'orderby'=> 'date',
-  'order' => 'DESC',
-  'tag' => 'CAB'
-  ));
-?>
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'category_name' => 'latest', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
 
 <?php if ( $wpb_all_query->have_posts() ) : ?>
+
+<ul>
+
     <!-- the loop -->
     <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-        <h1><a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h1>
-        <p><?php the_content(); ?></p>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
     <?php endwhile; ?>
     <!-- end of the loop -->
+
+</ul>
+
     <?php wp_reset_postdata(); ?>
 
 <?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+  </div>
+  <div class="cab">
+    <h1>CAB Stuff</h1>
+    <?php
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'category_name' => 'cab', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+
+<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+<ul>
+
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+
+</ul>
+
+    <?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+  </div>
+  <div class="threat">
+    <h1>Threats!</h1>
+    <?php
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'category_name' => 'threat', 'post_status'=>'publish', 'posts_per_page'=>-1)); ?>
+
+<?php if ( $wpb_all_query->have_posts() ) : ?>
+
+<ul>
+
+    <!-- the loop -->
+    <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+    <?php endwhile; ?>
+    <!-- end of the loop -->
+
+</ul>
+
+    <?php wp_reset_postdata(); ?>
+
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
 
   </div>
